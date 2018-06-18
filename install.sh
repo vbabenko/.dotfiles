c43b6ac3fd02d9c2d675321d462800fb0f83cdc4
Antony Peklo <antony.peklo@onereach.com>
2018-06-18 11:32:55 +0300

    improved install.sh to be more compatible

--- install.sh
+++ install.sh
@@ -1,20 +1,23 @@
 #!/bin/sh
 
+# If you come from bash you might have to change your $PATH.
+export PATH=$HOME/bin:/usr/local/bin:/usr/local/sbin:$PATH
+
 echo "Setting up your Mac..."
 
-export DOTFILES=$HOME/.dotfiles
+[[ -z "$DOTFILES" ]] && export DOTFILES="$HOME/.dotfiles"
 
 # Setup Homebrew with all needed receipes, casks
-source install/brew
+source install/brew &&
 
 # Setup zsh with plugins and .zshrc
-source install/zsh
-
-# Restore application configs
-source install/mackup
+source install/zsh &&
 
 # Set NVM with needed versions and global npm packages
-source install/nvm
+source install/nvm &&
+
+# Restore application configs
+source install/mackup &&
 
 # Set macOS preferences
 # We will run this last because this will reload the shell
