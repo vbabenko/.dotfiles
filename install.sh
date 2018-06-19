9a1f66665060f90aa5e2e0d6a7e6c2d8f7fe8105
Antony Peklo <antony.peklo@onereach.com>
2018-06-19 12:51:25 +0300

    updated install.sh to be more robust

--- install.sh
+++ install.sh
@@ -8,17 +8,17 @@ echo "Setting up your Mac..."
 [[ -z "$DOTFILES" ]] && export DOTFILES="$HOME/.dotfiles"
 
 # Setup Homebrew with all needed receipes, casks
-source install/brew &&
+source $DOTFILES/install/brew &&
 
 # Setup zsh with plugins and .zshrc
-source install/zsh &&
+source $DOTFILES/install/zsh &&
 
 # Set NVM with needed versions and global npm packages
-source install/nvm &&
+source $DOTFILES/install/nvm &&
 
 # Restore application configs
-source install/mackup &&
+source $DOTFILES/install/mackup
 
 # Set macOS preferences
 # We will run this last because this will reload the shell
-source install/macos
+source $DOTFILES/install/macos
